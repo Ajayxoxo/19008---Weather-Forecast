63311887bf449dc4183315394e991c4ff5cc8b92
Ajayxoxo <123183656+Ajayxoxo@users.noreply.github.com>
2025-05-25 19:29:51 +0530

    Got current conditions

--- server/index.ts
+++ server/index.ts
@@ -9,53 +9,35 @@ const PORT = 3000;
 
 app.use(cors());
 
-interface WeatherCondition {
-    description?: {
-        text?: string;
-    };
-    iconBaseUri?: string;
-}
 
-interface ForecastHour {
-    weatherCondition?: WeatherCondition;
-    temperature?: {
-        degrees?: number;
-    };
-    relativeHumidity?: number;
-}
-
-interface WeatherApiResponse {
-    forecastHours: ForecastHour[];
-}
-
-interface Forecast {
-    condition: string;
-    temperature: number;
-    humidity: number;
-    icon: string;
+interface currentConditions {
     label: string;
+    probability: number; 
+    qpf: number;
 }
 
 
 app.get('/api/weather', async (req: Request, res: Response) => {
     const apiKey = process.env.WEATHER_API_KEY;
-    const lat: string = (req.query.lat as string) || '19.167688';
-    const lon: string = (req.query.lon as string) || '72.848275';
+    const lat: string = (req.query.lat as string) || '19.043246';
+    const lon: string = (req.query.lon as string) || '72.857086';
 
-    const url = `https://weather.googleapis.com/v1/forecast/hours:lookup?key=${apiKey}&location.latitude=${lat}&location.longitude=${lon}&hours=1`;
+    //const url = `https://weather.googleapis.com/v1/currentConditions:lookup?key=AIzaSyCBe0cnqJ8G2EgoLTe7tihFcfu3BApA488&location.latitude=19.043246&location.longitude=72.857086`;
+    const url = `https://weather.googleapis.com/v1/currentConditions:lookup?key=${apiKey}&location.latitude=${lat}&location.longitude=${lon}`;
+    
+    //const url = `https://weather.googleapis.com/v1/forecast/hours:lookup?key=${apiKey}&location.latitude=${lat}&location.longitude=${lon}&hours=1`;
 
     try {
         const response = await axios.get(url);
+        const data = response.data;
         console.log(response);
-        const forecast: Forecast[] = (response.data as WeatherApiResponse).forecastHours.map((hour: ForecastHour) => ({
-            condition: hour.weatherCondition?.description?.text || "Unknown",
-            temperature: hour.temperature?.degrees ?? 0,
-            humidity: hour.relativeHumidity ?? 0,
-            icon: hour.weatherCondition?.iconBaseUri + ".svg" || "",
-            label: "poda punnaku",
-        }));
+        const currentCondition: currentConditions = {
+            label:" just for debugging ",
+            probability: data.precipitation?.probability?.percent,
+            qpf: data.currentConditionsHistory?.qpf?.quantity,
+        }
 
-        res.json(forecast);
+        res.json(currentCondition);
     } catch (error: any) {
         console.error("Error fetching weather:", error.message);
         res.status(500).send("Weather fetch failed");
